a4c0d1df8c3a1a39e0df6ffa5834a5c16d8fe919
Bryan Robinson <bryanlrobinson@gmail.com>
2024-11-04 09:06:45 -0500

    Updates title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "The website of one Bryan Robinson. Lots of web development, design, and education content.";
+export const SITE_DESCRIPTION = "I educate folks about the web and technology. You may have heard of me, but probably not (I'm not that arrogant). I'm a developer educator that has been working on the web for over 18 years. I've done everything from video product, to leading development and design teams, to building the frontend for enterprise, content-heavy websites.";
