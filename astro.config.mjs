a167a1d17fda1b568e4d835bf11dd11446cf430c
Bryan Robinson <bryanlrobinson@gmail.com>
2024-11-15 21:06:14 -0500

    Adds server island for testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bryanlrobinson.com',
   integrations: [mdx(), sitemap(), react(), tailwind()],
-  experimental: {
-    contentLayer: true
-  }
+
+
+  adapter: netlify()
 });
