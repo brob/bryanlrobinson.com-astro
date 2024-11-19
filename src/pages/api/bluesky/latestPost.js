6de85c0d0c546771d458c7966f978c1c86f561a9
Bryan Robinson <bryanlrobinson@gmail.com>
2024-11-19 14:18:02 -0500

    Tries function caching with island

--- src/pages/api/bluesky/latestPost.js
+++ src/pages/api/bluesky/latestPost.js
@@ -4,19 +4,19 @@ import { CacheHeaders } from "cdn-cache-control";
 export const prerender = false;
 
 export async function GET({ params }) {
-    const id = params.id;
-
     const {data} = await agent.getAuthorFeed({
         actor: 'brob.dev'
     })
 
     const latestPost = data.feed[0].post
-  
+    const headers = new CacheHeaders().ttl(300).swr(600);
+    console.log(JSON.stringify(Object.fromEntries([...headers])))
     return new Response(
       JSON.stringify(latestPost), {
         status: 200,
         headers: {
           "Content-Type": "application/json",
+          headers
         }
       }
     );
