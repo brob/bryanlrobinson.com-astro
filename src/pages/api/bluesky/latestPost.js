448de9ad155f78f20ba2ef7f34c2589e6ef3d9f3
Bryan Robinson <bryanlrobinson@gmail.com>
2024-11-20 09:17:00 -0500

    Shows Astro.response.headers in template

--- src/pages/api/bluesky/latestPost.js
+++ src/pages/api/bluesky/latestPost.js
@@ -12,7 +12,7 @@ export async function GET({ params }) {
     const headers = new CacheHeaders().ttl(300);
     console.log('In latestPost.js')
     console.log(headers)
-    console.log(JSON.stringify(Object.fromEntries([...headers])))
+
     const theResponse = new Response(
         JSON.stringify(latestPost), {
             status: 200,
